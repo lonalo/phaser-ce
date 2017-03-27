4f42adee99140b1d5cb4ff53bb81045fda8450e6
Dries Allemeersch <allemeerschdries@gmail.com>
2017-03-27 10:15:53 +0200

    init (based on Phaser CE v2.7.5)

--- Gruntfile.js
+++ Gruntfile.js
@@ -570,5 +570,18 @@ module.exports = function (grunt) {
         grunt.task.run('custom');
 
     });
+	
+	grunt.registerTask('mobile', 'Phaser for e-games', function() {
+
+      grunt.option('exclude', 'gamepad,keyboard,bitmaptext,retrofont,net,arcade,ninja,p2,tilemaps,particles,creature,video,weapon');
+      grunt.option('filename', 'phaser-mobile');
+      grunt.option('sourcemap', true);
+      grunt.option('copy', false);
+      grunt.option('copycustom', true);
+      grunt.option('uglify', true);
+	  grunt.option('split', false);
+      grunt.task.run('custom');
+
+    });
 
 };
